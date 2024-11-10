19ebea2827eda88acab21927e4e5745223a8976d
gmyasoedov <gmyasoedov@yandex.ru>
2024-11-10 14:14:31 +0300

    chore: module 'gmaven-execution-event-spy' move to gmaven plugin repo

--- gmaven-execution-event-spy/src/main/java/ru/rzn/gmyasoedov/evemt/spy/GMavenEventSpyConstants.java
+++
@@ -1,7 +0,0 @@
-package ru.rzn.gmyasoedov.evemt.spy;
-
-public interface GMavenEventSpyConstants {
-    String GMAVEN_DEPENDENCY_GRAPH = "gmaven-dependency-graph";
-    String MAVEN_MODEL_READER_PLUGIN = "maven-model-reader-plugin";
-    String RESULT_FILE_PATH = "resultFilePath";
-}
